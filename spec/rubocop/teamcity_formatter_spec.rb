26105d316018e61ec2dd19c97869b783f6937d7a
yanyee wong <wong_yan_yee@tech.gov.sg>
2021-09-19 00:20:17 +0800

    Updated rubocop specs with newer rubocop

--- spec/rubocop/teamcity_formatter_spec.rb
+++ spec/rubocop/teamcity_formatter_spec.rb
@@ -1,6 +1,9 @@
-# rubocop:disable Style/FileName
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/BlockLength
+
 require 'spec_helper'
-require 'rubocop/formatter/teamcity-formatter'
+require 'rubocop/formatter/teamcity_formatter'
 
 RSpec.describe RuboCop::Formatter::TeamCityFormatter do
   subject { RuboCop::Formatter::TeamCityFormatter.new(Kernel) }
@@ -33,7 +36,7 @@ RSpec.describe RuboCop::Formatter::TeamCityFormatter do
         Struct::TestStruct.new('Lint/AmbiguousOperator',
                                'cop1 error1',
                                "#{Dir.pwd}/foo.rb:1:1"),
-        Struct::TestStruct.new('Lint/BlockAlignment',
+        Struct::TestStruct.new('Layout/BlockAlignment',
                                'cop2 error1',
                                "#{Dir.pwd}/foo.rb:2:1"),
         Struct::TestStruct.new('Lint/AmbiguousOperator',
@@ -46,15 +49,15 @@ RSpec.describe RuboCop::Formatter::TeamCityFormatter do
       expected_output = '' \
         "##teamcity[testStarted name='somefile.txt']\n" \
         "##teamcity[testFailed name='somefile.txt' " \
+        "message='foo.rb:2:1: cop2 error1']\n" \
+        "##teamcity[testFinished name='somefile.txt']\n" \
+        "##teamcity[testStarted name='somefile.txt']\n" \
+        "##teamcity[testFailed name='somefile.txt' " \
         "message='foo.rb:1:1: cop1 error1']\n" \
         "##teamcity[testFinished name='somefile.txt']\n" \
         "##teamcity[testStarted name='somefile.txt']\n" \
         "##teamcity[testFailed name='somefile.txt' " \
         "message='foo.rb:1:2: cop1 error2']\n" \
-        "##teamcity[testFinished name='somefile.txt']\n" \
-        "##teamcity[testStarted name='somefile.txt']\n" \
-        "##teamcity[testFailed name='somefile.txt' " \
-        "message='foo.rb:2:1: cop2 error1']\n" \
         "##teamcity[testFinished name='somefile.txt']\n"
 
       expect do
@@ -63,3 +66,5 @@ RSpec.describe RuboCop::Formatter::TeamCityFormatter do
     end
   end
 end
+
+# rubocop:enable Metrics/BlockLength
